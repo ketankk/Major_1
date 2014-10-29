c8e45edf64e58a9fbfa9c4c1f39cdee1545d69be
sultan <ketank90@gmail.com>
2014-10-28 22:09:21 -0500

    Almost Final

---
+++ WindowsFormsApplication4/Form2.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WindowsFormsApplication4
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form2));
+            this.button1 = new System.Windows.Forms.Button();
+            this.hello = new System.Windows.Forms.Label();
+            this.message = new System.Windows.Forms.RichTextBox();
+            this.title = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(414, 228);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Post";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // hello
+            // 
+            this.hello.AutoSize = true;
+            this.hello.Location = new System.Drawing.Point(454, 53);
+            this.hello.Name = "hello";
+            this.hello.Size = new System.Drawing.Size(0, 13);
+            this.hello.TabIndex = 3;
+            // 
+            // message
+            // 
+            this.message.ForeColor = System.Drawing.Color.Gray;
+            this.message.Location = new System.Drawing.Point(55, 198);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(343, 140);
+            this.message.TabIndex = 3;
+            this.message.Text = "Type your message here";
+            this.message.MouseClick += new System.Windows.Forms.MouseEventHandler(this.message_MouseClick);
+            this.message.TextChanged += new System.EventHandler(this.message_TextChanged);
+            this.message.MouseEnter += new System.EventHandler(this.message_MouseEnter);
+            // 
+            // title
+            // 
+            this.title.ForeColor = System.Drawing.Color.Gray;
+            this.title.Location = new System.Drawing.Point(55, 143);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(143, 20);
+            this.title.TabIndex = 1;
+            this.title.Text = "Title";
+            this.title.MouseClick += new System.Windows.Forms.MouseEventHandler(this.title_MouseClick);
+            this.title.MouseEnter += new System.EventHandler(this.title_MouseEnter);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApplication4.Properties.Resources.logo1;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(107, 104);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 387);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "© Copy Right 2014";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(310, 387);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "To be used only for Departmental purpose";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "I",
+            "II",
+            "III",
+            "IV",
+            "ALL"});
+            this.comboBox1.Location = new System.Drawing.Point(277, 142);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 2;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Mistral", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(454, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 19);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(135, 78);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(242, 18);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Department of Applied Mathematics";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(135, 26);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(242, 18);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Delhi Technological University";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Franklin Gothic Book", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(411, 148);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(61, 15);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "Select Year";
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(564, 462);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.title);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.hello);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(580, 500);
+            this.MinimumSize = new System.Drawing.Size(580, 500);
+            this.Name = "Form2";
+            this.Text = "DashBoard";
+            this.Load += new System.EventHandler(this.Form2_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label hello;
+        private System.Windows.Forms.RichTextBox message;
+        private System.Windows.Forms.TextBox title;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
---
+++ WindowsFormsApplication4/Form2.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.IO;
+using System.Net;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication4
+{
+    public partial class Form2 : Form
+    {
+        public Form2(string user)
+        {
+            InitializeComponent();
+            label1.Text = "Hello "+user;
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void title_OnClick(object sender, EventArgs e)
+        {
+            
+        }
+
+       
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (title.Text == ""||title.Text=="Title")
+            {
+                MessageBox.Show("Please Enter the Title of Message");
+            }
+            else
+            if (message.Text == "" || message.Text == "Type your message here")
+            {
+                MessageBox.Show("Enter Message");
+            }
+            else
+                if (comboBox1.Text == "")
+                {
+                    MessageBox.Show("Plea select Year");
+                }
+                else
+                {
+                    string tag = "login";
+                    string content = "title=" + title.Text + "&message=" + message.Text + "&year=" + comboBox1.Text;
+
+                    string URL = "http://localhost/pro/note/index.php";
+                    string URI = "http://k3k.bugs3.com/note/index.php";
+
+                    HttpWebRequest request = (HttpWebRequest)WebRequest.Create(URI);
+                    request.Method = "POST";
+                    byte[] byteArray = Encoding.UTF8.GetBytes(content);
+                    request.ContentType = "application/x-www-form-urlencoded";
+                    request.ContentLength = byteArray.Length;
+                    Stream dataStream = request.GetRequestStream();
+
+                    dataStream.Write(byteArray, 0, byteArray.Length);
+                    dataStream.Close();
+                    WebResponse response = request.GetResponse();
+                    dataStream = response.GetResponseStream();
+                    StreamReader reader = new StreamReader(dataStream);
+                    string responseFromServer = HttpUtility.UrlDecode(reader.ReadToEnd()).Split('<')[0];
+                    
+
+
+                    MessageBox.Show(responseFromServer);
+                }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void title_MouseClick(object sender, MouseEventArgs e)
+        {
+            title.Text = "";
+            title.ForeColor = Color.Black;
+        }
+
+       
+
+        private void message_MouseClick(object sender, MouseEventArgs e)
+        {
+            message.Text = "";
+            message.ForeColor = Color.Black;
+        }
+
+        private void message_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       private void message_MouseEnter(object sender, EventArgs e)
+        {
+            
+        }
+
+       private void title_MouseEnter(object sender, EventArgs e)
+       {
+           message.Text = "";
+           message.ForeColor = Color.Black;
+       }
+        
+    }
+}
