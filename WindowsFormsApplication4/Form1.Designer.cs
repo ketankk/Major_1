09c8b300685952d6547a2eeaaf54db882c1da3cb
scarescrow <sagnik.sinharoy@gmail.com>
2014-12-01 07:08:42 +0530

    Last Notice feature left

--- WindowsFormsApplication4/Form1.Designer.cs
+++ WindowsFormsApplication4/Form1.Designer.cs
@@ -45,7 +45,7 @@
             // 
             // login
             // 
-            this.login.Location = new System.Drawing.Point(211, 298);
+            this.login.Location = new System.Drawing.Point(206, 246);
             this.login.Name = "login";
             this.login.Size = new System.Drawing.Size(75, 23);
             this.login.TabIndex = 3;
@@ -56,12 +56,11 @@
             // email
             // 
             this.email.AutoSize = true;
-            this.email.Location = new System.Drawing.Point(97, 151);
+            this.email.Location = new System.Drawing.Point(143, 154);
             this.email.Name = "email";
-            this.email.Size = new System.Drawing.Size(32, 13);
+            this.email.Size = new System.Drawing.Size(35, 13);
             this.email.TabIndex = 1;
-            this.email.Text = "Email";
-            this.email.Click += new System.EventHandler(this.email_Click);
+            this.email.Text = "Email:";
             // 
             // email1
             // 
@@ -69,17 +68,15 @@
             this.email1.Name = "email1";
             this.email1.Size = new System.Drawing.Size(171, 20);
             this.email1.TabIndex = 0;
-            this.email1.TextChanged += new System.EventHandler(this.email1_TextChanged);
             // 
             // password
             // 
             this.password.AutoSize = true;
-            this.password.Location = new System.Drawing.Point(87, 231);
+            this.password.Location = new System.Drawing.Point(125, 194);
             this.password.Name = "password";
-            this.password.Size = new System.Drawing.Size(53, 13);
+            this.password.Size = new System.Drawing.Size(56, 13);
             this.password.TabIndex = 5;
-            this.password.Text = "Password";
-            this.password.Click += new System.EventHandler(this.password_Click);
+            this.password.Text = "Password:";
             // 
             // header
             // 
@@ -90,32 +87,30 @@
             this.header.Size = new System.Drawing.Size(242, 18);
             this.header.TabIndex = 6;
             this.header.Text = "Delhi Technological University";
-            this.header.Click += new System.EventHandler(this.label1_Click);
             // 
             // dept
             // 
             this.dept.AutoSize = true;
             this.dept.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.dept.Location = new System.Drawing.Point(137, 79);
+            this.dept.Location = new System.Drawing.Point(137, 60);
             this.dept.Name = "dept";
             this.dept.Size = new System.Drawing.Size(242, 18);
             this.dept.TabIndex = 7;
             this.dept.Text = "Department of Applied Mathematics";
-            this.dept.Click += new System.EventHandler(this.dept_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 394);
+            this.label2.Location = new System.Drawing.Point(17, 309);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(98, 13);
+            this.label2.Size = new System.Drawing.Size(90, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "© Copy Right 2014";
+            this.label2.Text = "© Copyright 2014";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(222, 394);
+            this.label3.Location = new System.Drawing.Point(288, 309);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(205, 13);
             this.label3.TabIndex = 9;
@@ -123,12 +118,11 @@
             // 
             // pass
             // 
-            this.pass.Location = new System.Drawing.Point(181, 231);
+            this.pass.Location = new System.Drawing.Point(181, 191);
             this.pass.Name = "pass";
             this.pass.PasswordChar = '•';
             this.pass.Size = new System.Drawing.Size(171, 20);
             this.pass.TabIndex = 1;
-            this.pass.TextChanged += new System.EventHandler(this.pass_TextChanged);
             // 
             // pictureBox1
             // 
@@ -138,7 +132,6 @@
             this.pictureBox1.Size = new System.Drawing.Size(103, 99);
             this.pictureBox1.TabIndex = 3;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // label1
             // 
@@ -155,7 +148,7 @@
             this.AcceptButton = this.login;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(564, 462);
+            this.ClientSize = new System.Drawing.Size(527, 348);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pass);
             this.Controls.Add(this.label3);
@@ -170,7 +163,6 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
